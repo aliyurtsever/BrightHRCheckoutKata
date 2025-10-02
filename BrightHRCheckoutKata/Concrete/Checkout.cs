0259aa44bc8bf74044bf4067f76db4fdebffd470
Ali Yurtsever <ali.yrtsvr@gmail.com>
2025-10-02 22:17:38 +0100

    refactored for default price rules

--- BrightHRCheckoutKata/Concrete/Checkout.cs
+++ BrightHRCheckoutKata/Concrete/Checkout.cs
@@ -7,7 +7,7 @@ namespace BrightHRCheckoutKata.Concrete
         private readonly Basket _basket = new();
         private readonly List<IPricingRule> _pricingRules;
 
-        public Checkout() : this(GetDefaultPricingRules())
+        public Checkout() : this(DefaultPriceRule.GetDefaultPricingRules())
         {
 
         }
@@ -39,16 +39,6 @@ namespace BrightHRCheckoutKata.Concrete
             {
                 _basket.Add(item);
             }               
-        }
-
-        private static IEnumerable<IPricingRule> GetDefaultPricingRules()
-        {
-            return new List<IPricingRule> {
-            new SpecialPriceRule("A", 50, 3, 130),
-            new SpecialPriceRule("B", 30, 2, 45),
-            new UnitPriceRule("C", 20),
-            new UnitPriceRule("D", 15)
-            };
         }
     }
 }
---
+++ BrightHRCheckoutKata/Concrete/DefaultPriceRule.cs
@@ -0,0 +1,22 @@
+﻿using BrightHRCheckoutKata.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrightHRCheckoutKata.Concrete
+{
+    public static class DefaultPriceRule
+    {
+        public static IEnumerable<IPricingRule> GetDefaultPricingRules()
+        {
+            return new List<IPricingRule> {
+            new SpecialPriceRule("A", 50, 3, 130),
+            new SpecialPriceRule("B", 30, 2, 45),
+            new UnitPriceRule("C", 20),
+            new UnitPriceRule("D", 15)
+            };
+        }
+    }
+}
