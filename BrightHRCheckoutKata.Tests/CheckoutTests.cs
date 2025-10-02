d6c6b26e79eda7d2ec673d21308b944d5b8ef742
Ali Yurtsever <ali.yrtsvr@gmail.com>
2025-10-02 19:59:38 +0100

    Add project files.

---
+++ BrightHRCheckoutKata.Tests/CheckoutTests.cs
@@ -0,0 +1,15 @@
+﻿
+namespace BrightHRCheckoutKata.Tests
+{
+    [TestFixture]
+    public class CheckoutTests
+    {
+        [Test]
+        public void CanCreateAnInstanceOfCheckout()
+        {
+            var checkout = new Checkout();
+
+            Assert.That(checkout, Does.Not.Null);
+        }
+    }
+}
