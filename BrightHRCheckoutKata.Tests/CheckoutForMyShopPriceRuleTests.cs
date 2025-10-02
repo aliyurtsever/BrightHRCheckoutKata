eaf4e2b66c0f4a72fe33fb5eb3a591302fed2128
Ali Yurtsever <ali.yrtsvr@gmail.com>
2025-10-02 23:15:18 +0100

    I just wanted to show If wanted to add new business rule is easy to add, implement IPrincingRule and define its own rules.
    Also I add 2 test for this.
    
    All test are passed

---
+++ BrightHRCheckoutKata.Tests/CheckoutForMyShopPriceRuleTests.cs
@@ -0,0 +1,55 @@
+﻿using BrightHRCheckoutKata.Abstract;
+using BrightHRCheckoutKata.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrightHRCheckoutKata.Tests
+{
+    [TestFixture]
+    public class CheckoutForMyShopPriceRuleTests
+    {
+        [Test]
+        public void GetTotalPrice_ScannedForMyShopPriceRule_Returns50()
+        {
+            var customPricingRule = new List<IPricingRule>{
+                                    new UnitPriceRule("A", 50),
+                                    new SpecialPriceRule("B", 30,3,60),
+                                    new MyShopPriceRule("C",20,4,60,5),
+                            };
+
+            var checkout = new Checkout(customPricingRule);
+
+            checkout.Scan("A");
+            checkout.Scan("B");
+            checkout.Scan("C");
+            var total = checkout.GetTotalPrice();
+
+            Assert.That(total, Is.EqualTo(100));
+        }
+
+        [Test]
+        public void GetTotalPrice_ScannedSingleBItem_Returns30()
+        {
+            var customPricingRule = new List<IPricingRule>{
+                                    new UnitPriceRule("A", 50),
+                                    new SpecialPriceRule("B", 30,3,60),
+                                    new MyShopPriceRule("C",20,4,60,5),
+                            };
+
+            var checkout = new Checkout(customPricingRule);
+
+            checkout.Scan("A");
+            checkout.Scan("B");
+            checkout.Scan("C");
+            checkout.Scan("C");
+            checkout.Scan("C");
+            checkout.Scan("C");
+            var total = checkout.GetTotalPrice();
+
+            Assert.That(total, Is.EqualTo(137));
+        }
+    }
+}
---
+++ BrightHRCheckoutKata/Concrete/MyShopPriceRule.cs
@@ -0,0 +1,44 @@
+﻿using BrightHRCheckoutKata.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrightHRCheckoutKata.Concrete
+{
+    public class MyShopPriceRule : IPricingRule
+    {
+        private readonly string _sku;
+        private readonly int _unitPrice;
+        private readonly int _offerQuantity;
+        private readonly int _offerPrice;
+        private readonly int _additionalDiscount;
+
+        public MyShopPriceRule(string sku, int unitPrice, int offerQuantity, int offerPrice, int additionalDiscount)
+        {
+            _sku = sku;
+            _unitPrice = unitPrice;
+            _offerQuantity = offerQuantity;
+            _offerPrice = offerPrice;
+            _additionalDiscount = additionalDiscount;//give additional X percent discount if offer quantity matches
+        }
+
+        public int CalculatePrice(int quantity)
+        {
+            int offerCount = quantity / _offerQuantity;
+            int remainder = quantity % _offerQuantity;
+            int total = offerCount * _offerPrice + remainder * _unitPrice;
+
+            if(offerCount > 0)
+            {
+                decimal discount = total * (_additionalDiscount / 100m);
+                total -= (int)discount;// we normally use decimal for prices but in this case it is int so I convert it to int.
+            }
+           
+            return total;
+        }
+
+        public bool AppliesTo(string sku) => sku == _sku;
+    }
+}
